12aed6c8ffb97a98485fc2e10851fdd1b3d85596
Hamza El-Kebir <ha.elkebir@gmail.com>
2025-03-14 11:53:52 -0500

    Initial commit

---
+++ SensorData.hpp
@@ -0,0 +1,42 @@
+//
+// Created by Hamza El-Kebir on 2/7/25.
+//
+
+#ifndef VIPER_SENSORDATA_HPP
+#define VIPER_SENSORDATA_HPP
+
+#include <cstddef>
+#include <cmath>
+
+#define DEG_TO_RAD (float) (M_PI / 180.0)
+
+// Define the SensorData struct
+struct SensorData {
+    int sensor_id;
+    long long time;
+    float x, y, z;
+    float nx, ny, nz;
+
+    static void convertFromEuler(SensorData &data);
+
+    static void normalizeNormal(SensorData &data);
+};
+
+struct SensorDataExtended {
+    int sensor_id;
+    long long time;
+    float x, y, z;
+    float nx, ny, nz;
+    float vx, vy, vz;  // Velocity for position
+    float vnx, vny, vnz; // Velocity for normal vector
+
+    SensorData convert() const;
+
+    // Convert velocity data
+    SensorData convertV() const;
+
+    static void normalizeNormal(SensorDataExtended &data);
+};
+
+
+#endif //VIPER_SENSORDATA_HPP
---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "viper_ui.h"
+#include "ZMQServer.hpp"
+#include "PointCloudGUI.hpp"
+
+int main() {
+//    std::cout << "HEMISPHERE_CONFIG size " << sizeof(HEMISPHERE_CONFIG) << std::endl;
+//    viper_ui ui;
+//    ui.detect_input();
+
+//    ZMQServer::init(5555, ZMQServer::Publisher);
+//    ZMQServer::serve();
+
+//    ZMQServer::init(5555, ZMQServer::Subscriber);
+//    ZMQServer::subscribeToSensor(1);
+//    ZMQServer::subscribeToSensor(2);
+//    ZMQServer::subscribeToSensor(3);
+//    ZMQServer::subscribeToSensor(4);
+//    ZMQServer::consume();
+
+//    ZMQServer::verbose = false;
+//    ZMQServer::init(5555, ZMQServer::Subscriber);
+//    ZMQServer::subscribeToSensor(1);
+//    ZMQServer::subscribeToSensor(2);
+//    ZMQServer::subscribeToSensor(3);
+//    ZMQServer::subscribeToSensor(4);
+//    ZMQServer::log();
+
+    ZMQServer::init(5555, ZMQServer::Subscriber);
+    ZMQServer::subscribeToSensor(1);
+    ZMQServer::subscribeToSensor(2);
+//    ZMQServer::subscribeToSensor(3);
+//    ZMQServer::subscribeToSensor(4);
+    PointCloudGUI::launchRealTime();
+
+    return 0;
+}
