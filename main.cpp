f579f143b1811184ff2f7d0daa45d613e3d43547
thihanmoekyaw <tkyaw2@illinois.edu>
2025-07-09 15:15:26 -0500

    Change main.cpp to include viper_ui

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@
 #include "PointCloudGUI.hpp"
 
 int main() {
-//    std::cout << "HEMISPHERE_CONFIG size " << sizeof(HEMISPHERE_CONFIG) << std::endl;
-//    viper_ui ui;
-//    ui.detect_input();
+   std::cout << "HEMISPHERE_CONFIG size " << sizeof(HEMISPHERE_CONFIG) << std::endl;
+   viper_ui ui;
+   ui.detect_input();
 
    ZMQServer::init(5555, ZMQServer::Publisher);
    ZMQServer::serve();
-x
+
 //    ZMQServer::init(5555, ZMQServer::Subscriber);
 //    ZMQServer::subscribeToSensor(1);
 //    ZMQServer::subscribeToSensor(2);
